e9a59aa1673a536858bce65b7cf13f469235bb54
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-20 04:31:41 +0000

    Add Button component and integrate it into the Landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,16 @@
 import Card from "@/components/Card"
-
+import Button from '../components/Button'
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      {/* Sizes */}
+      <div className="flex space-x-3">
+        <Button title="Small" className="text-sm px-2 py-1" />
+        <Button title="Medium" className="text-base px-4 py-2" />
+        <Button title="Large" className="text-lg px-6 py-3" />
+      </div>
     </div>
   )
 }
